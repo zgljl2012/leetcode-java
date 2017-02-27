6cf826b67b1578624a30e72042e6a5084aebacd6
zgljl2012 <2693491512@qq.com>
2017-02-27 09:45:33 +0800

    完成第31题

--- src/main/java/com/ex1_50/_31_NextPermutation.java
+++ src/main/java/com/ex1_50/_31_NextPermutation.java
@@ -2,15 +2,53 @@ package com.ex1_50;
 
 import com.base.Base;
 
+/**
+ * url：https://leetcode.com/problems/next-permutation/?tab=Description
+ * 
+ * @author {author name}
+ *
+ */
 public class _31_NextPermutation extends Base {
 	
 	public static void main(String[] args) {
-		
+		int[] nums = {1,1,5};
+		Solution s = new Solution();
+		s.nextPermutation(nums);
+		println(nums);
+		s.nextPermutation(nums);
+		println(nums);
 	}
 	
 	static public class Solution {
 	    public void nextPermutation(int[] nums) {
+	    	if(nums==null||nums.length==0) return;
+	        int pos = nums.length-1;
+	        while(pos>0&&nums[pos-1]>=nums[pos]) {
+	        	pos--;
+	        }
+	        if(pos==0) {
+	        	reverse(nums, 0, nums.length-1);
+	        	return;
+	        }
+	        int i = nums.length-1;
+	        while(i>pos&&nums[i]<=nums[pos-1]) {
+	        	i--;
+	        }
+	        swap(nums, i, pos-1); // 找到pos-1在后半截的位置并置入，此时后半截已经是降序排序的数组了
+	        reverse(nums, pos, nums.length-1);
 	        
+	    }
+	    
+	    public void swap(int[] nums, int i, int j) {
+	    	int tmp = nums[i];
+	    	nums[i] = nums[j];
+	    	nums[j] = tmp;
+	    }
+	    
+	    public void reverse(int[] nums, int i, int j) {
+	    	while(i<j) {
+	    		swap(nums, i++, j--);
+	    	}
 	    }
 	}
 
